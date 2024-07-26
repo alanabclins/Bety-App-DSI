607b0a0783b8783c97961b8e9f1d5df19f5d3ffa
Alana Lins <alanacamaralins@gmail.com>
2024-07-26 09:33:54 -0300

    testando o git

--- bety_app/lib/main.dart
+++ bety_app/lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         fontFamily: 'Poppins',
       ),
-      home: NotificacoesScreen(),
+      home: LoginScreen(),
     );
   }
 }
