6abab9d5b63e40e6ebf63016ce70c17c87d23df0
Alana Lins <alanacamaralins@gmail.com>
2024-07-26 09:24:07 -0300

    Caminho notificacao

--- bety_app/lib/main.dart
+++ bety_app/lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         fontFamily: 'Poppins',
       ),
-      home: LoginScreen(),
+      home: NotificacoesScreen(),
     );
   }
 }
