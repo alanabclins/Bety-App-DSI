2c93ac05b979dccb22613e84c6528bbab514a29e
David Erick <googserick@gmail.com>
2024-09-05 01:05:34 -0300

    bug fixes

--- bety_app/lib/screens/login_screen.dart
+++ bety_app/lib/screens/login_screen.dart
@@ -23,157 +23,175 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: const Color.fromARGB(255, 251, 250, 243),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Image.asset(
-              "assets/betyLogo.png",
-              width: 120,
-              height: 120,
-            ),
-            const Text(
-              "Bety",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 11, 171, 124),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(30),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        labelText: "Seu email aqui",
-                        filled: true,
-                        fillColor: const Color.fromARGB(255, 199, 244, 194),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Por favor, insira seu email';
-                        }
-                        if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                          return 'Por favor, insira um email válido';
-                        }
-                        return null;
-                      },
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context)
+            .unfocus(), // Esconde o teclado ao tocar fora do campo de texto
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context)
+                .size
+                .height, // Garante que o Container ocupe a tela inteira
+            color: const Color.fromARGB(255, 251, 250, 243),
+            child: Center(
+              // Centraliza o conteúdo
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Image.asset(
+                    "assets/betyLogo.png",
+                    width: 120,
+                    height: 120,
+                  ),
+                  const Text(
+                    "Bety",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 11, 171, 124),
                     ),
-                    const SizedBox(height: 35),
-                    TextFormField(
-                      keyboardType: TextInputType.visiblePassword,
-                      obscureText: !_senhaVisivel,
-                      controller: _senhaController,
-                      decoration: InputDecoration(
-                        labelText: "Sua senha aqui",
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _senhaVisivel
-                                ? Icons.visibility
-                                : Icons.visibility_off,
-                            color: const Color.fromARGB(255, 11, 171, 124),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(30),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                              labelText: "Seu email aqui",
+                              filled: true,
+                              fillColor:
+                                  const Color.fromARGB(255, 199, 244, 194),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Por favor, insira seu email';
+                              }
+                              if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                                  .hasMatch(value)) {
+                                return 'Por favor, insira um email válido';
+                              }
+                              return null;
+                            },
                           ),
-                          onPressed: () {
-                            setState(() {
-                              _senhaVisivel = !_senhaVisivel;
-                            });
-                          },
-                        ),
-                        filled: true,
-                        fillColor: const Color.fromARGB(255, 199, 244, 194),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Por favor, insira sua senha';
-                        }
-                        if (value.length < 6) {
-                          return 'A senha deve ter pelo menos 6 caracteres';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    const SizedBox(height: 35),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                            User? user = await _authService.signIn(
-                              email: _emailController.text,
-                              password: _senhaController.text,
-                            );
-
-                            if (user != null) {
-                              Navigator.pushReplacementNamed(context, '/home');
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text('Usuário ou senha inválida'),
+                          const SizedBox(height: 35),
+                          TextFormField(
+                            keyboardType: TextInputType.visiblePassword,
+                            obscureText: !_senhaVisivel,
+                            controller: _senhaController,
+                            decoration: InputDecoration(
+                              labelText: "Sua senha aqui",
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _senhaVisivel
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                  color:
+                                      const Color.fromARGB(255, 11, 171, 124),
                                 ),
-                              );
-                            }
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.all(15),
-                          textStyle: const TextStyle(fontSize: 20),
-                          foregroundColor:
-                              const Color.fromARGB(255, 199, 244, 194),
-                          backgroundColor:
-                              const Color.fromARGB(255, 11, 171, 124),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
+                                onPressed: () {
+                                  setState(() {
+                                    _senhaVisivel = !_senhaVisivel;
+                                  });
+                                },
+                              ),
+                              filled: true,
+                              fillColor:
+                                  const Color.fromARGB(255, 199, 244, 194),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Por favor, insira sua senha';
+                              }
+                              if (value.length < 6) {
+                                return 'A senha deve ter pelo menos 6 caracteres';
+                              }
+                              return null;
+                            },
                           ),
-                        ),
-                        child: const Text('Entrar'),
+                          const SizedBox(height: 10),
+                          const SizedBox(height: 35),
+                          SizedBox(
+                            width: double.infinity,
+                            height: 50,
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                if (_formKey.currentState!.validate()) {
+                                  User? user = await _authService.signIn(
+                                    email: _emailController.text,
+                                    password: _senhaController.text,
+                                  );
+
+                                  if (user != null) {
+                                    Navigator.pushReplacementNamed(
+                                        context, '/home');
+                                  } else {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content:
+                                            Text('Usuário ou senha inválida'),
+                                      ),
+                                    );
+                                  }
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                padding: const EdgeInsets.all(15),
+                                textStyle: const TextStyle(fontSize: 20),
+                                foregroundColor:
+                                    const Color.fromARGB(255, 199, 244, 194),
+                                backgroundColor:
+                                    const Color.fromARGB(255, 11, 171, 124),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              child: const Text('Entrar'),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/cadastro');
+                            },
+                            child: const Text(
+                              "Para se cadastrar clique aqui",
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Color.fromARGB(255, 11, 171, 124),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/recuperar_senha');
+                            },
+                            child: const Text(
+                              "Esqueceu a senha?",
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 11, 171, 124),
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    const SizedBox(height: 10),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/cadastro');
-                      },
-                      child: const Text(
-                        "Para se cadastrar clique aqui",
-                        style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          color: Color.fromARGB(255, 11, 171, 124),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/recuperar_senha');
-                      },
-                      child: const Text(
-                        "Esqueceu a senha?",
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 11, 171, 124),
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
